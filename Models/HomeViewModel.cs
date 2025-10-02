d53ad130b1137687dabc6a7976752edf93a76240
odinellefsen <odin@ellefsen.fo>
2025-10-02 10:01:03 +0100

    Add HomeViewModel for home page data and remove Privacy and Error views

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace FacilityManagement.Models
+{
+    public class HomeViewModel
+    {
+        public string Message { get; set; } = "Welcome to my first MVC page!";
+        public DateTime Today { get; set; } = DateTime.Now;
+    }
+}
