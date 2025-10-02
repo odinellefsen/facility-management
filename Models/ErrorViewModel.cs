ef9957547b8b548f4bc56091732ae0a78292024f
odinellefsen <odin@ellefsen.fo>
2025-10-02 09:42:04 +0100

    Remove HomeController and ErrorViewModel classes from the project

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FacilityManagement.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
